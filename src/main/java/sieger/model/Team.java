bbea6bb4fbdf7eaeaadf49480808d67e8f772e43
Chen Zhang <utgws@student.kit.edu>
2021-02-01 00:11:07 +0100

    team finished

--- src/main/java/sieger/model/Team.java
+++ src/main/java/sieger/model/Team.java
@@ -1,5 +1,101 @@
 package sieger.model;
 
-public class Team {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+public class Team extends Participant{
+	//team id
+	private String teamId;
+	//admin id
+	private String adminId;
+	//team name
+	private String teamName;
+	//password of team
+	private String teamPassword;
+	//member list
+	private List<String> memberList;
+	//constructor
+	public Team(String adminId, String name, String password) {
+		this.adminId = adminId;
+		this.teamName = name;
+		this.teamPassword = password;
+		this.teamId = randomId();
+		this.memberList = new ArrayList<>();
+	}
+	//get random Id
+	private String randomId() {
+		return UUID.randomUUID().toString();
+	}
+	//implement getparticipantname()
+	@Override
+	public String getParticipantName() {
+		return this.teamName;
+	}
+	//implement joinTournament()
+	@Override
+	public boolean joinTournament(Tournament tournament) {
+		if(tournament.allowTeamToJoin()) {
+			addTournament(tournament.getTournamentId());
+			tournament.addParticipant(teamId);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	//implement quitTournament()
+	@Override
+	public boolean quitTournament(Tournament tournament) {
+		removeTournament(tournament.getTournamentId());
+		tournament.removeParticipant(teamId);
+		return true;
+	}
+	//check if password is true
+	public boolean checkPassword(String password) {
+		if(password.equals(teamPassword)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	//check if user is Admin
+	public boolean checkAdmin(User admin) {
+		if(admin.getUserId().equals(adminId)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	//kick member
+	public void kickMember(User user) {
+		
+	}
+	//add member
+	public void addMember(String userId) {
+		memberList.add(userId);
+	}
+	//remove member
+	public void removeMember(String userId) {
+		memberList.remove(userId);
+	}
+	//get teamid
+	public String getTeamId() {
+		return this.teamId;
+	}
+	//get adminId
+	public String getAdminId() {
+		return this.adminId;
+	}
+	//get team name
+	public String getTeamName() {
+		return this.teamName;
+	}
+	//get team password
+	public String getTeamPassword() {
+		return this.teamPassword;
+	}
+	//get memberlist
+	public List<String> getMemberList(){
+		return this.memberList;
+	}
 }
--- src/main/java/sieger/model/User.java
+++ src/main/java/sieger/model/User.java
@@ -1,5 +1,138 @@
 package sieger.model;
 
-public class User {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+public class User extends Participant{
+	//user id
+	private String userId;
+	//user name
+	private String userName;
+	//surname
+	private String surname;
+	//forname
+	private String forename;
+	//notificationlist
+	private List<Notification> notificationList;
+	//list of team
+	private List<String> teamList;
+	//list of invitation
+	private List<String> invitationList;
+	//constructor
+	public User(String userName, String surname, String forename) {
+		this.userName = userName;
+		this.forename = forename;
+		this.surname = surname;
+		this.userId = randomId();
+		this.notificationList = new ArrayList<>();
+		this.teamList = new ArrayList<>();
+		this.invitationList = new ArrayList<>();
+	}
+	//get random Id
+	private String randomId() {
+		return UUID.randomUUID().toString();
+	}
+	//implement getparticipantname()
+	@Override
+	public String getParticipantName() {
+		return this.userName;
+	}
+	//implement joinTournament
+	@Override
+	public boolean joinTournament(Tournament tournament) {
+		if(tournament.allowUserToJoin()) {
+			addTournament(tournament.getTournamentId());
+			tournament.addParticipant(userId);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	//implement quittournament
+	@Override
+	public boolean quitTournament(Tournament tournament) {
+		removeTournament(tournament.getTournamentId());
+		tournament.removeParticipant(userId);
+		return true;
+	}
+	//show next Tournament(not finished)
+	public List<String> showNextTournaments(){
+		return getTournamentList();
+	}
+	//join team
+	public boolean joinTeam(Team team, String password) {
+		if(team.checkPassword(password)) {
+			team.addMember(userId);
+			addTeam(team.getTeamId());
+			return true;
+		} else {
+			return false;
+		}
+	}
+	//quit team
+	public boolean quitTeam(Team team) {
+		team.removeMember(userId);
+		removeTeam(team.getTeamId());
+		return true;
+	}
+	//cancel Tournament
+	public boolean cancelTournament(Tournament tournament, List<Participant> participants) {
+		for(Participant participant: participants) {
+			participant.removeTournament(tournament.getTournamentId());
+		}
+		return true;
+	}
+	//add notification
+	public void addNotification(Notification notification) {
+		notificationList.add(notification);
+	}
+	//remove notification
+	public void removeNotification(Notification notification) {
+		notificationList.remove(notification);
+	}
+	//add invitation
+	public void addInvitation(String invitationId) {
+		invitationList.add(invitationId);
+	}
+	//remove invitation
+	public void removeInvitation(String invitationId) {
+		invitationList.remove(invitationId);
+	}
+	//add team
+	public void addTeam(String teamId) {
+		teamList.add(teamId);
+	}
+	//remove team
+	public void removeTeam(String teamId) {
+		teamList.remove(teamId);
+	}
+	//get userid
+	public String getUserId() {
+		return this.userId;
+	}
+	//get username
+	public String getUserName() {
+		return this.userName;
+	}
+	//get surname
+	public String getSurname() {
+		return this.surname;
+	}
+	//get forename
+	public String getForename() {
+		return this.forename;
+	}
+	//get notificationlist
+	public List<Notification> getNotificationList(){
+		return this.notificationList;
+	}
+	//get teamlist
+	public List<String> getTeamList(){
+		return this.teamList;
+	}
+	//get invitation list
+	public List<String> getInvitationList(){
+		return this.invitationList;
+	}
 }
