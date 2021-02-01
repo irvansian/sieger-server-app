e8251c0ae06d8210c8a2fafde1b430bb9f88d6a6
irvansian <irvansian@gmail.com>
2021-02-01 22:00:16 +0100

    Update createUser in UserDatabase
    The previous implementation doesn't set the document Id in the database as the userId

--- src/main/java/sieger/repository/database/UserDatabase.java
+++ src/main/java/sieger/repository/database/UserDatabase.java
@@ -4,6 +4,8 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.cloud.FirestoreClient;
@@ -19,7 +21,7 @@ public class UserDatabase implements UserRepository {
 	@Override
 	public boolean createUser(User user) {
 		Firestore db = FirestoreClient.getFirestore();
-		db.collection(path).add(user);
+		db.collection(path).document(user.getUserId()).set(user);
 		return true;
 	}
 
