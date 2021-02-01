80fd1a5fadf895bfecaee4db3884571782230ff3
irvansian <irvansian@gmail.com>
2021-02-01 22:28:31 +0100

    Implement retrieveUserById in UserDatabase
    Exception not handled yet

--- src/main/java/sieger/repository/database/UserDatabase.java
+++ src/main/java/sieger/repository/database/UserDatabase.java
@@ -1,13 +1,13 @@
 package sieger.repository.database;
 
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 import org.springframework.stereotype.Repository;
 
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.cloud.FirestoreClient;
 
 import sieger.model.User;
@@ -27,8 +27,23 @@ public class UserDatabase implements UserRepository {
 
 	@Override
 	public Optional<User> retrieveUserById(String userId) {
-		// TODO Auto-generated method stub
-		return null;
+		User user = null;
+		Firestore db = FirestoreClient.getFirestore();
+		ApiFuture<DocumentSnapshot> future = db.collection(path)
+				.document(userId).get();
+		
+		try {
+			if (future.get().exists()) {
+				user = future.get().toObject(User.class);
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Optional.ofNullable(user);
 	}
 
 	@Override
