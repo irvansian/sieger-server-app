5d41a2f1f1e1311f2b93027b5ec6310997c2fd84
irvansian <irvansian@gmail.com>
2021-02-02 01:30:05 +0100

    Implement retrieveTournamentById in TournamentDatabase

--- src/main/java/sieger/repository/database/TournamentDatabase.java
+++ src/main/java/sieger/repository/database/TournamentDatabase.java
@@ -2,7 +2,10 @@ package sieger.repository.database;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.cloud.FirestoreClient;
 
@@ -14,8 +17,23 @@ public class TournamentDatabase implements TournamentRepository {
 
 	@Override
 	public Optional<Tournament> retrieveTournamentById(String tournamentId) {
-		// TODO Auto-generated method stub
-		return null;
+		Tournament tournament = null;
+		Firestore db = FirestoreClient.getFirestore();
+		ApiFuture<DocumentSnapshot> future = db.collection(path)
+				.document(tournamentId).get();
+		
+		try {
+			if (future.get().exists()) {
+				tournament = future.get().toObject(Tournament.class);
+			}
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return Optional.ofNullable(tournament);
 	}
 
 	@Override
