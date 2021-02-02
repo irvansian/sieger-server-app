a94c248828bc9fedb577c55d32d090fee5dba901
irvansian <irvansian@gmail.com>
2021-02-02 01:39:44 +0100

    Implement createGame in GameDatabase

---
+++ src/main/java/sieger/repository/database/GameDatabase.java
@@ -0,0 +1,48 @@
+package sieger.repository.database;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.cloud.FirestoreClient;
+
+import sieger.model.Game;
+import sieger.repository.GameRepository;
+
+public class GameDatabase implements GameRepository {
+	
+	private String path = "games";
+
+	@Override
+	public Optional<Game> retrieveGameById(String tournmanetId, String gameId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Game> retrieveMultipleGamesById(String[] ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean createGame(String tournamentId, Game game) {
+		Firestore db = FirestoreClient.getFirestore();
+		db.collection("tournaments").document(tournamentId)
+			.collection(path).document(game.getGameId()).set(game);
+		return true;
+	}
+
+	@Override
+	public boolean updateGame(String tournamentId, String gameId, Game game) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteGmae(String tournamentId, String gameId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+}
