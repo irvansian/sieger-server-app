67bd88baaf4d8e2196c06cfc8d822fc84a09221d
irvansian <irvansian@gmail.com>
2021-02-04 22:39:10 +0100

    Create InvitationDatabase class
    Make it implement InvitationRepository

---
+++ src/main/java/sieger/repository/database/InvitationDatabase.java
@@ -0,0 +1,26 @@
+package sieger.repository.database;
+
+import sieger.model.Invitation;
+import sieger.repository.InvitationRepository;
+
+public class InvitationDatabase implements InvitationRepository {
+
+	@Override
+	public boolean createInvitation(Invitation invitation) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean retrieveInvitationById(String invitationId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteInvitation(String invitationId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
