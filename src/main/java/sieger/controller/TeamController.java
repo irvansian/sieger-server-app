9331cf0d21d7e0cf7c59d7ad91f304b363486880
irvansian <irvansian@gmail.com>
2021-02-06 18:58:39 +0100

    Remove unnecessary imports

--- src/main/java/sieger/controller/TeamController.java
+++ src/main/java/sieger/controller/TeamController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -99,7 +98,6 @@ public class TeamController {
 				HttpStatus.SC_INTERNAL_SERVER_ERROR);
 		
 	}
-	
 	
 	@PostMapping("/{teamName}")
 	public ResponseEntity<String> handleMembership(String currentUserId, 
--- src/main/java/sieger/controller/UserController.java
+++ src/main/java/sieger/controller/UserController.java
@@ -1,13 +1,12 @@
 package sieger.controller;
 
 import java.util.List;
+
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.ExecutionException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
