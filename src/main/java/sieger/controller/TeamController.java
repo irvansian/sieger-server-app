bda934337bf80ed115c77cc400ebac4f6cae9aef
irvansian <irvansian@gmail.com>
2021-02-06 02:15:31 +0100

    Annotate the class TeamController

--- src/main/java/sieger/controller/TeamController.java
+++ src/main/java/sieger/controller/TeamController.java
@@ -2,14 +2,21 @@ package sieger.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import sieger.model.Team;
 import sieger.model.Tournament;
 import sieger.model.User;
 import sieger.service.TeamService;
 
+@RestController
+@RequestMapping("teams")
 public class TeamController {
 	private TeamService teamService;
 	
+	@Autowired
 	public TeamController(TeamService teamService) {
 		this.teamService = teamService;
 	}
