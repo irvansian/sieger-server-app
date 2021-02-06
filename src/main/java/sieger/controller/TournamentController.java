0a4b01348e1131722ae53e517fff6f28d95b6b6d
irvansian <irvansian@gmail.com>
2021-02-06 02:01:51 +0100

    Implement deleteTournament in TournamentController

--- src/main/java/sieger/controller/TournamentController.java
+++ src/main/java/sieger/controller/TournamentController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -88,8 +89,14 @@ public class TournamentController {
 				HttpStatus.SC_UNPROCESSABLE_ENTITY);
 	}
 	
-	public void deleteTournament(String tournamentId) {
-		
+	@DeleteMapping("/tournamentName")
+	public ResponseEntity<String> deleteTournament(
+			@PathVariable("tournamentName") String tournamentName) {
+		if (tournamentService.deleteTournament(tournamentName)) {
+			return ResponseEntity.ok(null);
+		}
+		return new ResponseEntity<String>(null, null, 
+				HttpStatus.SC_INTERNAL_SERVER_ERROR);
 	}
 	
 	
