61ff0ef48facdae116e519397bf9f522c35480b7
irvansian <irvansian@gmail.com>
2021-02-06 18:26:10 +0100

    Annotate InvitationController as RestController

--- src/main/java/sieger/controller/InvitationController.java
+++ src/main/java/sieger/controller/InvitationController.java
@@ -1,11 +1,18 @@
 package sieger.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import sieger.model.Invitation;
 import sieger.service.InvitationService;
 
+@RestController
+@RequestMapping("invitations")
 public class InvitationController {
 	private InvitationService invitationService;
 
+	@Autowired
 	public InvitationController(InvitationService invitationService) {
 		this.invitationService = invitationService;
 	}
