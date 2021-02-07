faa84a05ebc1ac9157cc95baa69210aac3802291
irvansian <irvansian@gmail.com>
2021-02-07 13:54:57 +0100

    Add retrieveTournamentParticipant method

--- src/main/java/sieger/repository/TournamentRepository.java
+++ src/main/java/sieger/repository/TournamentRepository.java
@@ -1,13 +1,17 @@
 package sieger.repository;
 
 import java.util.List;
+
 import java.util.Optional;
 
+import sieger.model.Participant;
+import sieger.model.ParticipantForm;
 import sieger.model.Tournament;
 
 public interface TournamentRepository {
 	Optional<Tournament> retrieveTournamentById(String tournamentId);
 	Optional<Tournament> retrieveTournamentByName(String tournamentName);
+	List<Participant> retrieveTournamentParticipants(String tournamentId, ParticipantForm pf);
 	List<Tournament> retrieveMultipleTournamentsByKeyword(String keyword);
 	boolean createTournament(Tournament tournament);
 	boolean updateTournament(String tournamentId, Tournament tournament);
