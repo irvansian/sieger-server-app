50930ba0ba4b4f4a276184545ae8b20db9384e19
irvansian <irvansian@gmail.com>
2021-02-07 15:42:43 +0100

    Implement createInvitation in InvitationService

--- src/main/java/sieger/service/InvitationService.java
+++ src/main/java/sieger/service/InvitationService.java
@@ -3,6 +3,7 @@ package sieger.service;
 import java.util.Optional;
 
 import sieger.model.Invitation;
+import sieger.model.User;
 import sieger.repository.InvitationRepository;
 
 public class InvitationService {
@@ -15,11 +16,21 @@ public class InvitationService {
 	}
 	
 	public Optional<Invitation> getInvitation(String invitationId) {
-		return null;
+		Optional<Invitation> invitationOpt = invitationRepository
+				.retrieveInvitationById(invitationId);
+		if (invitationOpt.isEmpty()) {
+			// throw resource not found exception
+		}
+ 		return invitationOpt; 
 	}
 	
-	public boolean createInvitation(Invitation invitation) {
-		return false;
+	public boolean createInvitation(String currentUserId, Invitation invitation) {
+		User user = userService.getUserById(currentUserId).get();
+		if (!user.getUserName().equals(invitation.getSenderUsername())) {
+			// throw forbidden exception
+		}
+		invitationRepository.createInvitation(invitation);
+		return true;
 	}
 	
 	public boolean acceptInvitation(String userId, String invitationId) {
