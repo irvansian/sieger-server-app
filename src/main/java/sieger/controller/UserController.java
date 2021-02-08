a7fb93cebe52de851ec739c58e54d2ec1d241f53
irvansian <irvansian@gmail.com>
2021-02-08 09:30:13 +0100

    Refactor TeamService

--- src/main/java/sieger/controller/UserController.java
+++ src/main/java/sieger/controller/UserController.java
@@ -3,7 +3,6 @@ package sieger.controller;
 import java.util.List;
 
 import java.util.Map;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
--- src/main/java/sieger/service/TeamService.java
+++ src/main/java/sieger/service/TeamService.java
@@ -1,17 +1,24 @@
 package sieger.service;
 
 import java.util.ArrayList;
+
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import sieger.exception.BadRequestException;
+import sieger.exception.ForbiddenException;
+import sieger.exception.ResourceNotFoundException;
 import sieger.model.Team;
 import sieger.model.Tournament;
 import sieger.model.User;
+import sieger.payload.ApiResponse;
+import sieger.payload.UserProfile;
 import sieger.repository.TeamRepository;
+import sieger.repository.TournamentRepository;
+import sieger.repository.UserRepository;
 
 @Service
 public class TeamService {
@@ -20,104 +27,139 @@ public class TeamService {
 	private TeamRepository teamRepository;
 	
 	@Autowired
-	private UserService userService;
+	@Qualifier("userDB")
+	private UserRepository userRepository;
 	
 	@Autowired
-	private TournamentService tournamentService;
+	@Qualifier("tournamentDB")
+	private TournamentRepository tournamentRepository;
 	
-	public Optional<Team> getTeamByName(String currentUserId, 
+	public Team getTeamByName(String currentUserId, 
 			String teamName) {
-		Optional<Team> teamOpt = teamRepository
-				.retrieveTeamByName(teamName);
-		if (teamOpt.isEmpty()) {
-			//throw resource not found exception
+		Team team = teamRepository.retrieveTeamByName(teamName)
+				.orElseThrow(() -> 
+				new ResourceNotFoundException("Team", "name", teamName));
+		if (!team.getMemberList().contains(currentUserId)) {
+			ApiResponse res = new ApiResponse(false, "You have no permission to "
+					+ "view team <" + teamName + ">");
+			throw new ForbiddenException(res);
 		}
-		if (!teamOpt.get().getMemberList().contains(currentUserId)) {
-			//throw forbidden exception
+		return team;
+	}
+	
+	public Team getTeamById(String currentUserId, String teamId) {
+		Team team = teamRepository.retrieveTeamById(teamId)
+				.orElseThrow(() -> 
+				new ResourceNotFoundException("Team", "id", teamId));
+		if (!team.getMemberList().contains(currentUserId)) {
+			ApiResponse res = new ApiResponse(false, "You have no permission to "
+					+ "view team with ID <" + teamId + ">");
+			throw new ForbiddenException(res);
 		}
-		return teamOpt;
+		return team;
 	}
 	
-	public Optional<Team> getTeamById(String teamId) {
-		return teamRepository.retrieveTeamById(teamId);
-	}
-	
-	public boolean createNewTeam(Team team) {
-		return teamRepository.createTeam(team);
-	}
-	
-	public boolean deleteTeam(String teamId) {
-		return teamRepository.deleteTeam(teamId);
-	}
-	
-	public List<User> getTeamMembers(String teamId) {
-		List<User> teamMember = new ArrayList<>();
-		Optional<Team> team = teamRepository.retrieveTeamById(teamId);
-		if(!team.isEmpty()) {
-			for(String memberId: team.get().getMemberList()) {
-				teamMember.add(userService.getUserById(memberId).get());
-			}
+	public Team createNewTeam(Team team) {
+		if (teamRepository.retrieveTeamByName(team.getTeamName()).isPresent()) {
+			ApiResponse response = new ApiResponse(false, "Team with the name <" 
+					+ team.getTeamName() + "> already exist.");
+			throw new BadRequestException(response);
 		}
-		return teamMember;
+		teamRepository.createTeam(team);
+		return team;
 	}
 	
-	public List<Tournament> getTeamTournaments(String teamId) {
-		List<Tournament> teamTournament = new ArrayList<>();
-		Optional<Team> team = teamRepository.retrieveTeamById(teamId);
-		if(!team.isEmpty()) {
-			for(String tournamentId: team.get().getTournamentList()) {
-				teamTournament.add(tournamentService.getTournamentById(teamId, tournamentId).get());
-			}
+	public ApiResponse deleteTeam(String currentUserId, String teamName) {
+		Team team = getTeamByName(currentUserId, teamName);
+		if (!team.getAdminId().equals(currentUserId)) {
+			ApiResponse res = new ApiResponse(false, "You have no permission to "
+					+ "delete team <" + teamName + ">");
+			throw new ForbiddenException(res);
+		}
+		teamRepository.deleteTeam(teamName);
+		return new ApiResponse(true, "You deleted the team <" + teamName + ">");	
+	}
+	
+	public List<UserProfile> getTeamMembers(String currentUserId, String teamName) {
+		List<UserProfile> members = new ArrayList<UserProfile>();
+		Team team = getTeamByName(currentUserId, teamName);
+		for (String id : team.getMemberList()) {
+			User user = userRepository.retrieveUserById(id).get();
+			UserProfile up = new UserProfile(user.getUserId(),
+					user.getForename(),
+					user.getSurname(),
+					user.getUserName());
+			members.add(up);
+		}
+		return members;
+	}
+	
+	public List<Tournament> getTeamTournaments(String currentUserId, 
+			String teamName) {
+		List<Tournament> teamTournament = new ArrayList<Tournament>();
+		Team team = getTeamByName(currentUserId, teamName);
+		for(String id: team.getTournamentList()) {
+			teamTournament.add(tournamentRepository.retrieveTournamentById(id).get());
 		}
 		return teamTournament;
 	}
 	
-	public boolean kickTeamMembers(String userId, String teamId) {
-		Optional<Team> team = teamRepository.retrieveTeamById(teamId);
-		User user = userService.getUserById(userId).get();
-		if(!team.isEmpty()) {
-			team.get().kickMember(user);
+	public ApiResponse kickTeamMembers(String currentUserId, 
+			String userToKickId, String teamName) {
+		Team team = getTeamByName(currentUserId, teamName);
+		User userToKick = userRepository.retrieveUserById(userToKickId)
+				.orElseThrow(() -> 
+				new ResourceNotFoundException("User", "id", userToKickId));
+		if (!team.getAdminId().equals(currentUserId)) {
+			ApiResponse res = new ApiResponse(false, "You have no permission to "
+					+ "kick a member.");
+			throw new ForbiddenException(res);
 		}
-		return userService.updateUserById(userId, user) && updateTeamById(teamId, team.get());
+		team.kickMember(userToKick);
+		teamRepository.updateTeam(team.getTeamId(), team);
+		userRepository.updateUserById(userToKick.getUserId(), userToKick);
+		ApiResponse res = new ApiResponse(true, "You kicked a member with ID <" 
+		+ userToKickId + ">");
+		return res;
 	}
 	
-	public boolean updateTeamById(String teamId, Team team) {
-		Optional<Team> retrievedTeam = getTeamById(teamId);
-		if (retrievedTeam.isEmpty()) return false;
-		teamRepository.updateTeam(teamId, team);
-		return true;
-	}
-	
-	public void joinTeam(String userId, String teamName, String teamPassword) {
-		Optional<Team> team = getTeamByName(userId, teamName);
-		Optional<User> user = userService.getUserById(userId);
-		if (team.isEmpty() || user.isEmpty()) {
-			//throw exception
-		}
+	public ApiResponse joinTeam(String currentUserId, String teamName, 
+			String teamPassword) {
+		User user = userRepository.retrieveUserById(currentUserId)
+				.orElseThrow(() -> new ResourceNotFoundException(
+						"User", "id", currentUserId));
+		Team team = teamRepository.retrieveTeamByName(teamName)
+				.orElseThrow(() -> new ResourceNotFoundException(
+						"Team", "name", teamName));
 		
-		boolean success = user.get().joinTeam(team.get(), teamPassword);
+		boolean success = user.joinTeam(team, teamPassword);
 		
 		if (!success) {
-			//throw an exception
+			ApiResponse response = new ApiResponse(false, "Failed to join the team.");
+			throw new BadRequestException(response);
 		}
-		userService.updateUserById(userId, user.get());
-		teamRepository.updateTeam(team.get().getTeamId(), team.get());
+		userRepository.updateUserById(currentUserId, user);
+		teamRepository.updateTeam(team.getTeamId(), team);
+		ApiResponse res = new ApiResponse(true, "Successfully joined the team");
+		return res;
 	}
 	
-	public void quitTeam(String userId, String teamName) {
-		Optional<Team> team = getTeamByName(userId,teamName);
-		Optional<User> user = userService.getUserById(userId);
-		if (team.isEmpty() || user.isEmpty()) {
-			//throw exception
-		}
+	public ApiResponse quitTeam(String currentUserId, String teamName) {
+		Team team = getTeamByName(currentUserId,teamName);
+		User user = userRepository.retrieveUserById(currentUserId)
+				.orElseThrow(() -> new ResourceNotFoundException(
+						"User", "id", currentUserId));
 		
-		boolean success = user.get().quitTeam(team.get());
+		boolean success = user.quitTeam(team);
 		
 		if (!success) {
-			//throw an exception
+			ApiResponse response = new ApiResponse(false, "Failed to quit the team.");
+			throw new BadRequestException(response);
 		}
-		userService.updateUserById(userId, user.get());
-		teamRepository.updateTeam(team.get().getTeamId(), team.get());
+		userRepository.updateUserById(currentUserId, user);
+		teamRepository.updateTeam(team.getTeamId(), team);
+		ApiResponse res = new ApiResponse(true, "Successfully quit the team");
+		return res;
 	}
 	
 }
