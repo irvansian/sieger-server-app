8b4e49bd5cca340b1185ac020c1c845478a8aa59
irvansian <irvansian@gmail.com>
2021-02-08 10:46:20 +0100

    Correct some methods

--- src/main/java/sieger/controller/InvitationController.java
+++ src/main/java/sieger/controller/InvitationController.java
@@ -1,6 +1,4 @@
 package sieger.controller;
-
-import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,22 +12,14 @@ import sieger.service.InvitationService;
 @RestController
 @RequestMapping("invitations")
 public class InvitationController {
-	private InvitationService invitationService;
-
 	@Autowired
-	public InvitationController(InvitationService invitationService) {
-		this.invitationService = invitationService;
-	}
+	private InvitationService invitationService;
 	
 	@PostMapping
-	public ResponseEntity<String> createInvitation(String currentUserId, 
+	public ResponseEntity<Invitation> createInvitation(String currentUserId, 
 			@RequestBody Invitation invitation) {
-		if (invitationService.createInvitation(currentUserId, invitation)) {
-			return ResponseEntity.ok(null);
-		}
-		
-		return new ResponseEntity<String>(null, null, 
-				HttpStatus.SC_UNPROCESSABLE_ENTITY);
+		Invitation invitationReady = invitationService.createInvitation(currentUserId, invitation);
+		return ResponseEntity.ok(invitationReady);
 	}
 	
 }
