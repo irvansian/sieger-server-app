035eb8f36c96e13aa3d80ef0926060f46fc819b2
irvansian <irvansian@gmail.com>
2021-02-08 02:28:53 +0100

    Change the HTTP status

--- src/main/java/sieger/exception/ForbiddenException.java
+++ src/main/java/sieger/exception/ForbiddenException.java
@@ -4,7 +4,7 @@ import sieger.payload.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+@ResponseStatus(code = HttpStatus.FORBIDDEN)
 public class ForbiddenException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 
