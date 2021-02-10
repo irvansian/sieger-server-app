5267cb50741223971d07ebf8f29e3fb7d2821f4b
irvansian <irvansian@gmail.com>
2021-02-10 20:43:04 +0100

    Create IdFilter for testing purpose with Postman

---
+++ src/main/java/sieger/filter/IdFilter.java
@@ -0,0 +1,23 @@
+package sieger.filter;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class IdFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		final String authorizationHeader = request.getHeader("ID");
+		request.setAttribute("currentUserId", authorizationHeader);
+		filterChain.doFilter(request, response);
+		
+	}
+
+}
