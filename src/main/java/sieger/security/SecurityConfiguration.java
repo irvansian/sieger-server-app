9256cb71122f78a10d8ebda184339a5e257a4d7a
irvansian <irvansian@gmail.com>
2021-02-10 20:37:43 +0100

    Delete the previous filters and adjust the security config

---
+++ src/main/java/sieger/security/SecurityConfiguration.java
@@ -0,0 +1,30 @@
+package sieger.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import sieger.filter.AuthFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private AuthFilter authFilter;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+        	.authorizeRequests().antMatchers("/").permitAll()
+        	.anyRequest().authenticated().and()
+        	.sessionManagement().sessionCreationPolicy(
+        			SessionCreationPolicy.STATELESS);
+        http.formLogin().disable();
+        http.addFilterBefore(authFilter, BasicAuthenticationFilter.class);
+    }
+}
