80dc7ec89bbe4e6a390559ad57b49e67cdb85ee5
Chen Zhang <utgws@student.kit.edu>
2021-02-11 20:51:50 +0100

    merged

--- src/main/java/sieger/SiegerApplication.java
+++ src/main/java/sieger/SiegerApplication.java
@@ -1,17 +1,6 @@
 package sieger;
 
 import org.springframework.boot.SpringApplication;
-<<<<<<< HEAD
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseToken;
-
-import sieger.controller.UserController;
-=======
->>>>>>> 47019cd93c2b13bd55afde2a19e671479214be2f
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
