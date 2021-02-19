27145d730b91a9b52107ef6660768d2053de327d
irvansian <irvansian@gmail.com>
2021-02-19 15:28:55 +0100

    Add Javadoc in ApiResponse and UserProfile

--- src/main/java/sieger/payload/ApiResponse.java
+++ src/main/java/sieger/payload/ApiResponse.java
@@ -8,6 +8,13 @@ import org.springframework.http.HttpStatus;
 
 import java.io.Serializable;
 
+/**
+ * This object of the class is going to be packed inside the HTTP Response Body.
+ * It contains the information if the request that the client sent was successful or not along
+ * with the message.
+ * @author Irvan Sian Syah Putra.
+ *
+ */
 @Data
 @JsonPropertyOrder({
 		"success",
@@ -15,27 +22,53 @@ import java.io.Serializable;
 })
 public class ApiResponse implements Serializable {
 
+	/**
+	 * Serial version UID for serialization.
+	 */
 	@JsonIgnore
 	private static final long serialVersionUID = 7702134516418120340L;
 
+	/**
+	 * Boolean of whether or not the request was successful.
+	 */
 	@JsonProperty("success")
 	private Boolean success;
 
+	/**
+	 * Message for client.
+	 */
 	@JsonProperty("message")
 	private String message;
 
+	/**
+	 * The Http Status for response.
+	 */
 	@JsonIgnore
 	private HttpStatus status;
 
+	/**
+	 * Empty constructor for ApiResponse.
+	 */
 	public ApiResponse() {
 
 	}
 
+	/**
+	 * Constructor for ApiResponse
+	 * @param success Whether or not the request was successful
+	 * @param message Message for client
+	 */
 	public ApiResponse(Boolean success, String message) {
 		this.success = success;
 		this.message = message;
 	}
 
+	/**
+	 * Constructor for ApiResponse
+	 * @param success Whether or not the request was successful
+	 * @param message Message for client
+	 * @param httpStatus Status for HTTP Response
+	 */
 	public ApiResponse(Boolean success, String message, HttpStatus httpStatus) {
 		this.success = success;
 		this.message = message;
--- src/main/java/sieger/payload/UserProfile.java
+++ src/main/java/sieger/payload/UserProfile.java
@@ -1,11 +1,24 @@
 package sieger.payload;
 
+/**
+ * When a user A tries to access user B resource, the user B resource will be
+ * converted to UserProfile, so that user A doesn't know private information of user B.
+ * @author Irvan Sian Syah Putra
+ *
+ */
 public class UserProfile {
 	private String id;
 	private String forename;
 	private String surname;
 	private String username;
 	
+	/**
+	 * Constructor for UserProfile.
+	 * @param id Id of the user
+	 * @param forename First name of the user
+	 * @param surname Last name of the user
+	 * @param username Username of the user
+	 */
 	public UserProfile(String id, String forename, String surname, String username) {
 		this.id = id;
 		this.forename = forename;
@@ -13,34 +26,66 @@ public class UserProfile {
 		this.username = username;
 	}
 
+	/**
+	 * Getter for Id
+	 * @return Id attribute
+	 */
 	public String getId() {
 		return id;
 	}
 
+	/**
+	 * Setter for Id
+	 * @param id Id to be passed
+	 */
 	public void setId(String id) {
 		this.id = id;
 	}
 
+	/**
+	 * Getter for forename attirbute.
+	 * @return forename
+	 */
 	public String getForename() {
 		return forename;
 	}
 
+	/**
+	 * Setter for forename attribute.
+	 * @param forename Forename to be passed
+	 */
 	public void setForename(String forename) {
 		this.forename = forename;
 	}
 
+	/**
+	 * Getter for surname.
+	 * @return surname
+	 */
 	public String getSurname() {
 		return surname;
 	}
 
+	/**
+	 * Setter for surname
+	 * @param surname Surname to be passed
+	 */
 	public void setSurname(String surname) {
 		this.surname = surname;
 	}
 
+	/**
+	 * Getter for username
+	 * @return username
+	 */
 	public String getUsername() {
 		return username;
 	}
 
+	/**
+	 * Setter for username
+	 * @param username Username to be passed.
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
