dbf0696bf0326c707ecd1c47d02d8da2c919976b
irvansian <irvansian@gmail.com>
2021-02-19 15:35:43 +0100

    Add Javadoc to SecurityConfiguration

--- src/main/java/sieger/security/SecurityConfiguration.java
+++ src/main/java/sieger/security/SecurityConfiguration.java
@@ -13,12 +13,23 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 //import sieger.filter.IdFilter;
 import sieger.filter.JwtFilter;
 
+/**
+ * The configuration for security of the server app.
+ * @author Irvan Sian Syah Putra
+ *
+ */
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
+	/**
+	 * The Filter that is used to catch the JWT Token in header.
+	 */
 	@Autowired
 	private JwtFilter authFilter;
 
+	/**
+	 * HTTP Security configuration.
+	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -29,10 +40,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.addFilterBefore(authFilter, BasicAuthenticationFilter.class);
     }
 	
+	/**
+	 * This configuration is used to prevent Spring Boot auto configuration.
+	 */
 	@Bean
 	public AuthenticationManager authenticationManagerBean() throws Exception {
-	    // ALTHOUGH THIS SEEMS LIKE USELESS CODE,
-	    // IT'S REQUIRED TO PREVENT SPRING BOOT AUTO-CONFIGURATION
 	    return super.authenticationManagerBean();
 	}
 }
